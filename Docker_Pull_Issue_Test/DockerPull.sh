b9b6b3d9a932f7dad3c4b8f64a1c9f681e2134ed
Rushabh Shah <rushabhshah842@gmail.com>
2024-07-25 16:03:12 +0530

    Corrected K3s Flow

--- Docker_Pull_Issue_Test/DockerPull.sh
+++ Docker_Pull_Issue_Test/DockerPull.sh
@@ -40,6 +40,7 @@ pull_image() {
 
 
 # Check Host Entries for Docker Registry | Check this for all Enviornments
+echo "\n\n Checking Host Entries for Docker Registry \n\n"
 grep -Fx "10.0.0.6 docker-registry-mirror.kodekloud.com" /etc/hosts
 if [ $? -eq 0 ]; then
     print_color "green" " \n Hosts File Entry Exists \n"
@@ -48,36 +49,42 @@ else
 fi
 
 # CHECK DOCKER
-which docker
+echo "which docker: $(which docker)"
 if [ $? -eq 0 ]; then
-    print_color "green" " \n Docker is Installed (Version: $(docker --version)) \n"
+    print_color "green" " \n Docker is Installed $(docker --version) \n"
     cat /etc/docker/daemon.json | grep "docker-registry-mirror.kodekloud.com"
-    print_color "green" " \n Checking For Docker Pull \n \n \n"
+    print_color "green" " \n Checking For Docker Pull \n\n"
     docker run docker/whalesay cowsay KodeKloud #Chcecking Docker pull
 else
-    print_color "red" "\n Docker is Not Installed \n\n\n"
-
-    print_color "green" "Checking For Kubernetes \n"
+    print_color "red" "\n Docker is Not Installed \n"
+    print_color "green" "\n Checking For Kubernetes \n"
 
     kubectl get all
-    echo "\n\n\n"
+    echo "\n"
     systemctl status containerd.service
     if [ $? -eq 0 ]; then
-        print_color "green" "Containerd Service is Running\n\n"
+        print_color "green" "\n Containerd Service is Running\n\n"
         grep -Fx "docker-registry-mirror.kodekloud.com" /etc/containerd/config.toml
         if [ $? -eq 0 ]; then
             print_color "green" "Config File Entry Exists at /etc/containerd/config.toml"
+            cat /etc/containerd/config.toml 
+            print_color "green" "\n\n\n Pulling Image Running Nginx Pod\n\n\n"
             pull_image nginx
         fi
     else  #This is for K3s Cluster
         print_color "red" "\n\n\n Containerd Service is Not Running | Not Exist | Not Installed \n \n \n "
-        print_color "red" "Config File Entry Does Not Exist in /etc/containerd/config.toml. Checking in /var/lib/rancher/k3s/agent/etc/containerd/certs.d"
+        print_color "green" "Checking in /var/lib/rancher/k3s/agent/etc/containerd/certs.d"
         ls -l /var/lib/rancher/k3s/agent/etc/containerd/certs.d | grep -Fx "docker-registry-mirror.kodekloud.com"
-        pull_image nginx
+        if [ $? -eq 0 ]; then
+            print_color "green" "Config File Entry Exists at /var/lib/rancher/k3s/agent/etc/containerd/certs.d"
+            ls -l /var/lib/rancher/k3s/agent/etc/containerd/certs.d
+            print_color "green" "\n\n\n Pulling Image Running Nginx Pod\n\n\n"
+            pull_image nginx
+        fi
     fi
 fi
 
-print_color "green" "===================================="
+print_color "green" "\n\n\n\n\n===================================="
 print_color "green" "     Docker Pull Test Completed     " 
 print_color "green" "         -Iamrushabhshahh-          "
 print_color "green" "===================================="
